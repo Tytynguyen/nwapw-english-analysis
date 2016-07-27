e63371c126b26fccb0e473513204dcd13274a970
Jacob Stein <stein.jacob.ao@gmail.com>
2016-07-27 14:54:06 -0700

    Merge ButtonFX and TextInput. GUI stores inputs to text feild in public variables

--- TextAnalysis/src/langanal/UserInterface/TextInput.java
+++ TextAnalysis/src/langanal/UserInterface/TextInput.java
@@ -5,17 +5,20 @@ import javax.swing.*;
 /**
  * Created by SteinJac.ao on 7/27/2016.
             */
-class TextInput extends JPanel /*implements ActionListener*/{
+class TextInput extends JPanel implements ActionListener {
 
     protected JTextField textField1;
     protected JTextField textField2;
+
+    protected JButton button;
+
+    public String text1;
+    public String text2;
 
     public TextInput(){
         super(new GridBagLayout());
 
         textField1 = new JTextField(20);
-
-        textField2 = new JTextField(20);
         textField2 = new JTextField(20);
 
         //Add componets to this panel
@@ -29,6 +32,18 @@ class TextInput extends JPanel /*implements ActionListener*/{
         c.fill = GridBagConstraints.BOTH;
         c.weightx = 1.0;
         c.weighty = 1.0;
+
+        button = new JButton("Compute");
+        button.setVerticalTextPosition(AbstractButton.CENTER);
+        button.setHorizontalTextPosition(AbstractButton.LEADING); //LEFT
+        button.setMnemonic(KeyEvent.VK_D);
+        button.setActionCommand("disable");
+
+        //Listen for actions on button
+        button.addActionListener(this);
+
+        //Add components to this container, using the default FlowLayout
+        add(button);
     }
 
 
@@ -42,17 +57,20 @@ class TextInput extends JPanel /*implements ActionListener*/{
         JFrame frame = new JFrame("TextInput");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Create and set up the content pane for button
-        ButtonFX newContentPane = new ButtonFX();
-        newContentPane.setOpaque(true); //content panes must be opaque
-        frame.setContentPane(newContentPane);
-
         //Add contents to the window
         frame.add(new TextInput());
 
         //Display the window
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(textField1.getText().equals("")||textField2.getText().equals("")){
+        }else{
+            text1 = textField1.getText();
+            text2 = textField2.getText();
+        }
     }
 
     public static void main(String[] args){
