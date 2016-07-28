d3490d5fe4e25fc1a9777dde43ce0a71c734ba62
edelmanjm <jonathanedelman0@gmail.com>
2016-07-28 09:28:24 -0700

    Basic SUMO loading functionality
    On construction it should now load SUMO into the main ontology
    
    Ignore walkOntology for now

--- TextAnalysis/src/langanal/owl/implementation/OwlProxy.java
+++ TextAnalysis/src/langanal/owl/implementation/OwlProxy.java
@@ -1,12 +1,43 @@
 package langanal.owl.implementation;
 
+import java.io.File;
+
 import org.semanticweb.owlapi.apibinding.*;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.util.*;
+import uk.ac.manchester.cs.owl.owlapi.*;
 
-public class OwlProxy {
+/**
+ * 
+ * @author Jonathan Edelman
+ *
+ */
+public class OWLProxy {
 
-    public OWLManager manager;
+	
+	File sumoXML = new File("TextAnalysis/src/langanal/owl/implementation/SUMO.owl");
+   
+	//cast should be fine since OWLOntologyManagerImpl implements OWLOntologyManager
+	private OWLOntologyManagerImpl manager = (OWLOntologyManagerImpl) OWLManager.createOWLOntologyManager();
+    private OWLOntology ontology;
 
-    public OwlProxy() {
-
+    /**
+     * 
+     */
+    public OWLProxy() {
+    	//must handle said exception
+    	try {
+    		manager.loadOntologyFromOntologyDocument(sumoXML);
+    	} catch (OWLOntologyCreationException e) {
+    		System.err.println(e);
+    	}
+    }
+    /**
+     * 
+     * 
+     * @return
+     */
+    public String walkOntology() {
+    	return null;
     }
 }
