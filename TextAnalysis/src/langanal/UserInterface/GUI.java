14b5cf4f6f3f20b117f2436c356a059daceb8203
Jacob Stein <stein.jacob.ao@gmail.com>
2016-08-02 14:00:28 -0700

    Updated GUI

--- TextAnalysis/src/langanal/UserInterface/GUI.java
+++ TextAnalysis/src/langanal/UserInterface/GUI.java
@@ -1,110 +1,129 @@
 package langanal.UserInterface;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 
 import langanal.word.processing.WordProcessing;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.*;
+
 /**
- * Created by SteinJac.ao on 7/27/2016.
-            */
-@SuppressWarnings("serial")
-class GUI extends JPanel implements ActionListener {
+ * Created by SteinJac.ao on 8/1/2016.
+ */
+public class GUI extends JPanel implements ActionListener {
 
     public String text1;
     public String text2;
     public float relevancy;
 
-    protected JTextField textField1;
-    protected JTextField textField2;
-
-    protected JTextArea textArea;
-    protected JTextArea appInfo; //brief description of what to do and what the application does
-
-    protected JButton button;
-
-    public GUI(){
-        super(new GridBagLayout());
-
-        GridBagConstraints c = new GridBagConstraints();
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        c.fill = GridBagConstraints.HORIZONTAL;
-
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1.0;
-        c.weighty = 1.0;
-
-        //text area for description
-        String description = "   Hello, this is an application designed to determine \n   the relativity of two words. " +
-                "Please enter one word \n   each in the areas below and press \"Compute\" ";
-        appInfo = new JTextArea(description,3,15);
-        appInfo.setEditable(false);
-        JScrollPane scrollPane2 = new JScrollPane(appInfo);
-        add(scrollPane2, c);
-
-        //text input fields
-        textField1 = new JTextField(20);
-        textField2 = new JTextField(20);
-        add(textField1, c);
-        add(textField2, c);
-
-        //button
-        button = new JButton("Compute");
-        button.setVerticalTextPosition(AbstractButton.CENTER);
-        button.setHorizontalTextPosition(AbstractButton.LEADING); //LEFT
-        button.setMnemonic(KeyEvent.VK_D);
-        button.setActionCommand("disable");
-        button.addActionListener(this);
-        add(button);
-
-        //text area for output
-        textArea = new JTextArea(TextOutput(relevancy),5,21);
-        textArea.setEditable(false);
-        JScrollPane scrollPane1 = new JScrollPane(textArea);
-        add(scrollPane1, c);
-    }
-
-
-    public static String TextOutput(float relevancyIn){
-        return " Percent Relevant: " + relevancyIn + "%" +
-                "\n Note: The higher the percentage, \n the more the two words have in common" +
-                "\n The lower the percentage, \n the less the two words have in common";
-    }
+    public JLabel instructions;//how to operate the application
+    public JTextArea inTA1;//input TextArea 1
+    public JTextArea inTA2;//iput TextArea 2
+    public JButton trigger;//initiates computations
+    public JTextArea feedback;//an output TextArea for relativity feedback
 
 
     /**
-     * Create the GUI and show it. For thread safety,
-     * this method shoudl be invoked form the
-     * event dispatch thread.
+     * @param Pane, JFrame frame will be taken in and given components
+     *
      */
-    private static void createAndShowGUI(){
-        //Create and set up the window
-        JFrame frame = new JFrame("GUI");
+    public void addComponents(Container Pane){
+        JPanel p = new JPanel();
+        //p.setBackground(new Color(156, 110, 92));
+        p.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(10,10,10,10);
+
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+        Color smokeWhite = new Color(245,245,245);
+
+        instructions = new JLabel("This application determines the relevancy of two texts. " +
+                "Please enter two texts in the two fields below");
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        p.add(instructions,c);
+
+        //definitions for inTA1
+        inTA1 = new JTextArea(5,0);
+        inTA1.setBorder(border);
+        inTA1.setBackground(smokeWhite);
+        c.gridy = 1;
+        p.add(inTA1, c);
+
+        //definitions for inTA2
+        inTA2 = new JTextArea(5,0);
+        inTA2.setBorder(border);
+        inTA2.setBackground(smokeWhite);
+        c.gridy = 2;
+        p.add(inTA2, c);
+
+        //definitions for trigger
+        trigger = new JButton("Compute");
+        c.gridy = 3;
+        c.fill = GridBagConstraints.CENTER;
+        trigger.setMnemonic(KeyEvent.VK_D);
+        trigger.setActionCommand("disable");
+        trigger.addActionListener(this);
+        p.add(trigger,c);
+
+        //definitions for
+        feedback = new JTextArea(10,0);
+        feedback.setBorder(border);
+        feedback.setBackground(smokeWhite);
+        feedback.setEditable(false);
+        c.gridy = 4;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        p.add(feedback,c);
+
+        Pane.add(p);
+
+    }
+
+    /**
+     * @param relevancy
+     * @return feedback string is given
+     */
+    public String TextOut(float relevancy){
+        return "" + relevancy;
+    }
+
+    /**
+     * creates the GUI
+     */
+    public void createAndShowGUI(){
+        JFrame frame = new JFrame("Text Relevancy Application");
+        frame.setSize(440,480);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.add(new GUI());
-
-        //Display the window
+        addComponents(frame);
         frame.pack();
         frame.setVisible(true);
     }
 
+    /**
+     * @param e listens for the button "trigger" to be clicked
+     */
     public void actionPerformed(ActionEvent e){
-        if(textField1.getText().equals("")||textField2.getText().equals("")){
-        }else {
-            text1 = textField1.getText();
-            text2 = textField2.getText();
+        if(!inTA1.getText().equals("") && !inTA2.getText().equals("")){
+            text1 = inTA1.getText();
+            text2 = inTA2.getText();
             this.relevancy = WordProcessing.compareWords(text1, text2);
-            textArea.setText(TextOutput(relevancy));
+            feedback.setText(TextOut(relevancy));
         }
     }
 
-    public static void main(String[] args){
-        //Schedule a job for the event dispatch thread:
-        //creating and shwoing theis application's GUI.
+    /**
+     * runs the applet
+     */
+    public static void main (String[] args){
+        GUI gui = new GUI();
         javax.swing.SwingUtilities.invokeLater(new Runnable(){
             public void run(){
-                        createAndShowGUI();
-                    }
+                gui.createAndShowGUI();
+            }
         });
     }
+
 }
