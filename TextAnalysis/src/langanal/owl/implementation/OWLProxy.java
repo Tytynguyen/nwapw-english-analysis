ae3dab74c15ccdd49cdbd1fae697ec701f735671
edelmanjm <jonathanedelman0@gmail.com>
2016-08-04 14:15:43 -0700

    degreesOfSeparation functionality
    walkOntology works to successfully find a given entry, with stepUp
    recursively getting the parents and remaining calculation functionality
    in degreesOfSepraration.

--- TextAnalysis/src/langanal/owl/implementation/OWLProxy.java
+++ TextAnalysis/src/langanal/owl/implementation/OWLProxy.java
@@ -15,7 +15,7 @@ import uk.ac.manchester.cs.owl.owlapi.*;
  */
 public class OWLProxy {
 
-	File sumoXML = new File("lib/SUMO.owl");
+	private File sumoXML = new File("lib/SUMO.owl");
 
    
 	//cast should be fine since OWLOntologyManagerImpl implements OWLOntologyManager
@@ -23,34 +23,121 @@ public class OWLProxy {
     private OWLOntology ontology;
 
     /**
-     * 
+     *
+	 * Constructs a basic OWLProxy and loads the SUMO.owl ontology
+	 *
      */
     public OWLProxy() {
+        //System.out.println("Started constructor");
+
     	//must handle said exception
     	try {
-    		manager.loadOntologyFromOntologyDocument(sumoXML);
+    		ontology = manager.loadOntologyFromOntologyDocument(sumoXML);
     	} catch (OWLOntologyCreationException e) {
     		System.err.println(e);
     	}
+
+    	//System.out.println("Finish loading");
+
     }
-    
-    //FIXME return type shouldn't be Strings as they're not a unique identifiers
+
     /**
      * 
-     * 
-     * @return
+     * Walks the ontology searching for a given item
+	 *
+	 * @param toFind has the word that needs to be found
+     * @return A List of OWLClasses beginning with the found expression and ending with the root expression, or null if couldn't find the word
      */
-    private ArrayList<String> walkOntology() {
-    	//create a new set with the only element being the main ontology
-    	//there's no need to use multiple sets here
-    	HashSet<OWLOntology> ontologySet = new HashSet<OWLOntology>();
-    	ontologySet.add(ontology);
-    	
-    	//create a new walker using ontologySet
-    	OWLOntologyWalker walker = new OWLOntologyWalker(ontologySet);
-    	
-    	
-    	return null;
-    }
+    private List<OWLClass> walkOntology(final String toFind) {
+
+		//create a LinkedList of OWLClasses that will be returned
+		LinkedList<OWLClassExpression> heirarchy = new LinkedList<OWLClassExpression>();
+
+		//for every class in the ontology
+		for (OWLClass currentClass:ontology.getClassesInSignature()) {
+
+			//currentWord is the super short lower case version of the full location of the currentClass
+			String currentWord = currentClass.getIRI().getShortForm();
+
+			//make the word lowercase
+			currentWord = currentWord.toLowerCase();
+
+			//System.out.println(currentWord);
+
+			//if it's the word we're looking for
+			if (currentWord.equals(toFind)) {
+				//System.out.println("Found word");
+				//call step up to find the parents
+				return stepUp(currentClass);
+			}
+		}
+
+		return null;
+
+	}
+
+	/**
+	 *
+	 * Recursive function that finds parents of a given class
+	 *
+	 * @param lastClass class to find parents of
+	 */
+	private LinkedList<OWLClass> stepUp(OWLClass lastClass) {
+
+		LinkedList<OWLClass> heirarchy = new LinkedList<OWLClass>();
+
+		heirarchy.add(lastClass);
+
+		//kludgy way of getting the first element
+		boolean hasRun = false;
+		for (OWLSubClassOfAxiom currentAxiom:ontology.getSubClassAxiomsForSubClass(lastClass.asOWLClass())) {
+			if (!hasRun) {
+				hasRun = true;
+				heirarchy.addAll(stepUp(currentAxiom.getSuperClass().asOWLClass()));
+			}
+		}
+		return heirarchy;
+	}
+
+    /**
+	 *
+	 * @param firstWord to compare
+	 * @param secondWord to compare
+	 * @return int based on the number of axioms needed to traverse from one word to another, or -1 if no commonality is found
+     */
+    public int degreesOfSeparation(String firstWord, String secondWord) {
+
+
+    	//gets the heirarchy
+		List<OWLClass> firstHeirarchy = walkOntology(firstWord);
+		List<OWLClass> secondHeirarchy = walkOntology(secondWord);
+
+		//if either is empty (no word found), return -1
+		if (firstHeirarchy == null | secondHeirarchy == null) {
+			return -1;
+		}
+
+		//System.out.println("––––––––––––––––––––––––––––––––––––––––––––––");
+
+		//determines degrees of separation
+
+		//find the closest commonality
+		//assumes iterator works front to back
+		for (OWLClass firstCurrent:firstHeirarchy) {
+			for (OWLClass secondCurrent:secondHeirarchy) {
+				//if they have a commonality
+				if (firstCurrent.equals(secondCurrent)) {
+					//number of elements left
+					int firstLeft = firstHeirarchy.indexOf(firstCurrent);
+					int secondLeft = firstHeirarchy.indexOf(secondCurrent);
+					//return the total
+					return firstLeft+secondLeft;
+				}
+			}
+		}
+
+		return -1;
+
+	}
 
 }
