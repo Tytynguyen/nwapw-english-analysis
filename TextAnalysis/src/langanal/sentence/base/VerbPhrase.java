343d85db3154e90b702e84fba8a397786fd89d7a
Jacob Stein <stein.jacob.ao@gmail.com>
2016-08-05 13:04:36 -0700

    updated VerbPhrase to work with processing

--- TextAnalysis/src/langanal/sentence/base/VerbPhrase.java
+++ TextAnalysis/src/langanal/sentence/base/VerbPhrase.java
@@ -1,12 +1,21 @@
 package langanal.sentence.base;
 
-import java.util.LinkedList;
-
 import edu.stanford.nlp.ling.IndexedWord;
+
+import java.util.LinkedList;
 
 public class VerbPhrase {
 	IndexedWord verb;
 	LinkedList<IndexedWord> modifiers;
-	
-	
+
+	public int getIndex(){return (int)verb.pseudoPosition()-1;}
+
+	public int[] getModIndex(){
+		int[] ModIndex = new int[modifiers.size()];
+		int i = 0;
+		for (IndexedWord mod: modifiers){
+			ModIndex[i] = (int)mod.pseudoPosition()-1;
+		}
+		return ModIndex;
+	}
 }
